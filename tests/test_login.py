d100da263cb6ea55f20b4437615f370da60ec154
JStrahan7813 <102590661+JStrahan7813@users.noreply.github.com>
2026-06-16 09:34:46 +0100

    Update test_login.py

--- tests/test_login.py
+++ tests/test_login.py
@@ -10,18 +10,18 @@ def driver():
     
     sauce_url = f"https://{sauce_username}:{sauce_access_key}@ondemand.eu-central-1.saucelabs.com:443/wd/hub"
     
-    print("\n📱 Initializing modern Appium 2.0 session on Sauce Labs RDC...")
+    print("\n📱 Initializing modern Appium session on Sauce Labs RDC...")
     
     options = UiAutomator2Options()
     options.platform_name = "Android"
     options.automation_name = "UiAutomator2"
     options.set_capability("appium:app", "https://github.com/saucelabs/my-demo-app-android/releases/download/v1.0.13/mda-1.0.13-15.apk")
     
-    # 🎯 APPIUM 2 COMPLIANCE DEFINITION BLOCK
+    # 🎯 APPIUM LATEST COMPLIANCE DEFINITION BLOCK
     options.set_capability("sauce:options", {
         "name": "Appium Real Device Mobile Portfolio Run",
-        "build": "Build-5.0",
-        "appiumVersion": "2.0.0"  # 👈 THIS SECURELY DEPLOYS APPIUM 2 ON SAUCE LABS
+        "build": "Build-6.0",
+        "appiumVersion": "latest"  # 👈 CHANGES THIS TO LATEST AS REQUESTED BY THE SERVER
     })
 
     driver = webdriver.Remote(sauce_url, options=options)
